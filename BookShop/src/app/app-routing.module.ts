5f1cf54255b7476eb61a9a2c55236d8efbf202f9
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-08-30 10:43:04 +0200

    Installazione App Angular

---
+++ BookShop/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./Pages/home/home.module').then(m => m.HomeModule),
+    title: "BookShop"
+  }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
